d847ff123337e08034205acfe6a45173c5d8292a
AmritShankarDutta <amrit08ju@gmail.com>
2026-04-12 12:40:43 +0530

    refactoring

--- app/service/embedding/EmbeddingFactory.py
+++ app/service/embedding/EmbeddingFactory.py
@@ -5,9 +5,11 @@ from typing import List
 from app.config.Settings import get_settings
 from app.service.embedding.MistralAIEmbeddingService import MistralAIEmbeddingService
 from app.service.embedding.genai_service import GenAIEmbeddingService
-from app.service.utils.resilience import gemini_breaker
 
 logger = logging.getLogger(__name__)
+
+_MistralAIEmbeddingService: MistralAIEmbeddingService | None = None
+_GenAIEmbeddingService: GenAIEmbeddingService | None = None
 
 
 async def get_query_embedding_async(text: str) -> List[float]:
@@ -21,19 +23,19 @@ async def get_query_embedding_async(text: str) -> List[float]:
 
 def get_embedding_service():
     settings = get_settings()
-
+    global _MistralAIEmbeddingService, _GenAIEmbeddingService
     # Check if primary embedder is genai and if its breaker is OPEN
     if settings.EMBEDDER == "genai":
-        if gemini_breaker.current_state == 'open':
-            logging.warning("Gemini breaker is OPEN. Rerouting embedding to MistralAI fallback.")
-            return MistralAIEmbeddingService()
-
         logging.info(
             f'embedding {settings.EMBEDDER} model to be used: {settings.EMBEDDING_MODEL}, DIMENSION: {settings.EMBEDDING_DIM}')
-        return GenAIEmbeddingService()
+        if _GenAIEmbeddingService is None:
+            _GenAIEmbeddingService = GenAIEmbeddingService()
+        return _GenAIEmbeddingService
     elif settings.EMBEDDER == "mistralai":
         logging.info(
             f'embedding {settings.EMBEDDER} model to be used: {settings.EMBEDDING_MODEL}, DIMENSION: {settings.EMBEDDING_DIM}')
-        return MistralAIEmbeddingService()
+        if _MistralAIEmbeddingService is None:
+            _MistralAIEmbeddingService = MistralAIEmbeddingService()
+        return _MistralAIEmbeddingService
 
     raise RuntimeError("unsupported embedder")
